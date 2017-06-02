88aa376e93ff4cce7748c68d8667cb6a80bb0b9b
Michael-Sui <suijiyuan1996@gmail.com>
2017-06-02 10:15:15 +0800

    添加过滤器

---
+++ src/main/java/filter/AuthorityFilter.java
@@ -0,0 +1,33 @@
+package filter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by Michael on 2017/6/2.
+ */
+@javax.servlet.annotation.WebFilter(filterName = "AuthorityFilter", urlPatterns = "/*")
+public class AuthorityFilter implements javax.servlet.Filter {
+    public void destroy() {}
+
+    public void doFilter(javax.servlet.ServletRequest request, javax.servlet.ServletResponse response,
+                         javax.servlet.FilterChain chain) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpSession httpSession = httpServletRequest.getSession();
+        String requestPath = httpServletRequest.getServletPath();
+        if (httpSession.getAttribute("user") == null &&
+                !requestPath.endsWith("/index.jsp") &&
+                !requestPath.endsWith("/page/login.jsp")) {
+            httpServletResponse.sendRedirect("/index.jsp");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    public void init(javax.servlet.FilterConfig config) throws javax.servlet.ServletException {}
+}
--- src/main/webapp/page/login.jsp
+++ src/main/webapp/page/login.jsp
@@ -8,7 +8,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>工资单管理系统</title>
+    <title>Title</title>
 </head>
 <body>
 
